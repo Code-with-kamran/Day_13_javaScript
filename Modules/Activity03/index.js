95be4bc4ecccc479b9cb2199a4fc486891cfcd50
kamran <147907811+Code-with-kamran@users.noreply.github.com>
2024-07-25 23:12:55 +0500

    Add files via upload

---
+++ Modules/Activity03/index.js
@@ -0,0 +1,16 @@
+const a=5;
+const b =6;
+const logName = ()=>{
+    console.log("Kamran");
+}
+const education = ()=>{
+    console.log("Bachlor's in computer science");
+}
+
+// whole module as an object
+export  const module1 = {
+    a:a,
+    b:b,
+    n:logName,
+    edu:education
+}
---
+++ Modules/Activity03/main.js
@@ -0,0 +1,2 @@
+import {module1} from "./index.js";
+console.log(module1.a);
